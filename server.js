0e2b6017eeb6e605c3c8cafb9be38417a51094a6
Casey <caseyderiso@gmail.com>
2020-12-04 11:57:31 -0600

    handled requests for search parameters to data

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const { animals } = require("./data/animals.json");
+
+app.listen(8080, () => {
+  console.log(`API server is now on port 8080!`);
+});
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  // set filtered results equal the whole data set
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    // save traits as an array
+    // if trait exists as a string, place it into a new array
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // loop through each trait in the personalityTraits array
+    personalityTraitsArray.forEach((trait) => {
+      filteredResults = filteredResults.filter(
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  // override the whole data set for a filter parameter (3 categories)
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  return filteredResults;
+}
+
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
